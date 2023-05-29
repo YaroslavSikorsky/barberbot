2b5df2252937d9019a700c5a222a191198166bf1
Yaroslav Sikorsky <yrl.sikorsky@gmail.com>
2023-05-29 20:40:25 +0300

    Change text in sout

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,16 @@ public class Main {
 	public static void main(String[] args) {
 
 		Salon salon = new Salon("Пушкина, 21");
-		Barber jura = new Barber("Юра");
+		Barber jura = new Barber("Jura");
 		salon.addBarber(jura);
 
-		Client anton = new Client("Антон", "111-111-1111");
+		Client anton = new Client("Anton", "111-111-1111");
 		Appointment appointment = new Appointment("01.01.2024", "10:00", jura, anton);
 
 		jura.addAppointment(appointment);
 		anton.addAppointment(appointment);
-//
-		System.out.println(jura.getName() + " назначена встреча " + anton.getName() + " c " + appointment.getDate() + " " + appointment.getTime() + ".");
+
+		System.out.println(jura.getName() + " vstre4a c " + anton.getName() + " na " + appointment.getDate() + " " + appointment.getTime() + ".");
 
 
 	}
